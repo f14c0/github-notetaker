7e50f322d869401757660ab92e2e71886d7c8826
Julian Vega <jcvgalfonso@gmail.com>
2016-01-10 16:36:02 -0500

    Add note Component
    React component composed as follows:
    -  input text
    -  submit button

---
+++ app/components/Notes/AddNote.js
@@ -0,0 +1,31 @@
+var React = require('react');
+
+var AddNote = React.createClass({
+    propTypes:{
+      username:React.PropTypes.string,
+      addNote:React.PropTypes.func.isRequired,
+    },
+    setRef:function (ref) {
+      this.note = ref;
+    },
+    handleSubmit:function () {
+      //Use .value  to get actual value of the prop
+      var newNote = this.note.value;
+      this.note.value='';
+      this.props.addNote(this.note.value);
+    },
+    render : function () {
+      return(
+        <div className= "input-group">
+            <input className="form-control"  type ="Text" ref ={this.setRef}>
+            </input>
+            <span className="input-group-btn">
+              <button  className ="btn btn-primary" onClick={this.handleSubmit}>Add Note</button>
+            </span>
+      </div>
+      )
+    }
+});
+
+//export component so we can use require to  use it
+module.exports = AddNote
