85b63e453ba02af6950ae70c84aef59d8c3a92c7
Julian Vega <jcvgalfonso@gmail.com>
2016-01-10 22:05:18 -0500

    Search User Component implemented
    Search user implemented, Input text and Submit button

---
+++ app/components/SearchUser.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var Router = require('react-router');
+
+var SearchUser = React.createClass({
+    mixins : [Router.History],
+    setRef : function (ref) {
+      this.userRef = ref;
+    },
+    handleSubmit :function () {
+        var username = this.userRef.value;
+        this.userRef.value ='';
+        this.history.pushState(null, "profile/" + username);
+    },
+    render : function () {
+      return(
+        <div className="panel panel-info">
+          <div className ="input-group">
+            <input className="form-control"  type ="Text" ref ={this.setRef}>
+            </input>
+            <span className="input-group-btn">
+              <button  className ="btn btn-primary" onClick={this.handleSubmit}>Search User</button>
+            </span>
+
+          </div>
+        </div>
+      )
+    }
+});
+
+//export component so we can use require to  use it
+module.exports = SearchUser
